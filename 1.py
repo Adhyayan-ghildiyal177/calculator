2dbac2adb00b3a7c49f8d8039940f876e04b5f5b
Adhyayan-ghildiyal177 <Adhyayanghildiyal177@gmail.com>
2026-05-07 22:41:10 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -48,3 +48,51 @@ while True:
     choice = input("Do you want to continue? (yes/no): ")
     if choice.lower() != "yes":
         break
+while True:
+
+    print("\n===== CALCULATOR =====")
+
+    try:
+        num1 = float(input("Enter first number: "))
+        op = input("Enter operator (+, -, *, /, %, **, //): ")
+        num2 = float(input("Enter second number: "))
+
+        if op == "+":
+            result = num1 + num2
+
+        elif op == "-":
+            result = num1 - num2
+
+        elif op == "*":
+            result = num1 * num2
+
+        elif op == "/":
+            if num2 != 0:
+                result = num1 / num2
+            else:
+                print("Cannot divide by zero")
+                continue
+
+        elif op == "%":
+            result = num1 % num2
+
+        elif op == "**":
+            result = num1 ** num2
+
+        elif op == "//":
+            result = num1 // num2
+
+        else:
+            print("Invalid operator")
+            continue
+
+        print("Answer =", result)
+
+    except ValueError:
+        print("Please enter valid numbers")
+
+    choice = input("\nDo you want to continue? (yes/no): ")
+
+    if choice.lower() != "yes":
+        print("Calculator closed")
+        break
